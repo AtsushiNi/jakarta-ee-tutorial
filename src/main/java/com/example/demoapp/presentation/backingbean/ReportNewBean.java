9d609318ff6dd8615b6150d23df39807b53d9fa2
AtsushiNi <atsushi9731@gmail.com>
2025-01-28 18:31:00 +0900

    form validation

--- src/main/java/com/example/demoapp/presentation/backingbean/ReportNewBean.java
+++ src/main/java/com/example/demoapp/presentation/backingbean/ReportNewBean.java
@@ -2,10 +2,13 @@ package com.example.demoapp.presentation.backingbean;
 
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
+import jakarta.validation.constraints.NotBlank;
 
 @Named
 @RequestScoped
 public class ReportNewBean {
+
+    @NotBlank(message = "タイトルは空白にできません")
     private String title;
     private String detail;
 
